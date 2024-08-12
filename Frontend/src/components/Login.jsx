a04add009c08c7d507f768704e87d50a603c550f
SIVANESH <sivanesh6363@gmail.com>
2024-08-12 20:32:33 +0530

    Added signup page

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import loginimg from "../assets/loginpageimg.png";
+import "./Login.css";
 export const Login = () => {
   return (
-    <></>
-  )
-}
+    <>
+    <body className="loginpagebody">
+          <div className="main-container">
+          <form action="" className="loginform">
+            <h1>Welcome Back</h1>
+            <label htmlFor="">Email Address</label>
+            <br />
+            <input type="text" />
+            <br />
+            <label htmlFor="">Password</label>
+            <br />
+            <input type="password" />
+            <br />
+            <p>
+              Lost Password? <span>click here</span>
+            </p>
+            <br />
+            <Link to="/"><button>Login</button></Link>
+            <br />
+            <p>
+              Don't have an account?{" "}
+              <Link to="/signup">
+                <span>Signup</span>
+              </Link>
+            </p>
+          </form>
+          <div className="img-container">
+            <img src={loginimg} alt="" />
+          </div>
+        </div>
+        </body>
+    </>
+  );
+};
--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -1,7 +1,49 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import loginimg from "../assets/loginpageimg.png";
+import "./Signup.css";
 export const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <>
+    <body className="Signuppagebody">
+          <div className="main-container">
+          <form action="" className="signupform">
+            <h1>Create an Account</h1>
+            <label htmlFor="">Email Address</label>
+            <br />
+            <input type="text" />
+            <br />
+            <label htmlFor="">Password</label>
+            <br />
+            <input type="password" />
+            <br />
+            <label htmlFor="">Confirm Password</label>
+            <br />
+            <input type="password" />
+            <br />
+            <label htmlFor="">Address</label>
+            <br />
+            <textarea  id="addresstextarea" rows="3" cols="40"></textarea>
+            <br />
+            <label htmlFor="">Pincode</label>
+            <br />
+            <input type="text" />
+            <br />
+            <br />
+            <Link to="/"><button>Sign Up</button></Link>
+            <br />
+            <p>
+              Already have an account?{" "}
+              <Link to="/login">
+                <span>Login</span>
+              </Link>
+            </p>
+          </form>
+          <div className="img-container">
+            <img src={loginimg} alt="" />
+          </div>
+        </div>
+        </body>
+    </>
+  );
+};
